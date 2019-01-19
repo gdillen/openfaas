bcca5fcad62e2ec8487e80150bf8da461b3b90f2
Guy Dillen <guy.dillen@infotechpartners.be>
2019-01-19 21:29:14 +0100

    Commit

---
+++ function1/FunctionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input) {
+            int value = DateTime.Now.Year;
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                dynamic jsonResponse = JsonConvert.DeserializeObject(input);
+                if (jsonResponse.value != null)
+                {
+                    value = (int) jsonResponse.value;
+                }
+            }
+        }
+    }
+}
